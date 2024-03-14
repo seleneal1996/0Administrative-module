f2bfab24cb8841cd8aea77929f7c56dc702845c3
Pejelagarta <selenemsb@hotmail.com>
2024-03-13 21:12:28 -0500

    add frontend

---
+++ frontend/src/config/constants.js
@@ -0,0 +1,18 @@
+const CONSTANTS = {
+  ROUTES: {
+    PUBLIC: {
+      SIGN_USER: { to: "/", label: "Cerrar Sesion" },
+    },
+    PRIVATE: {
+      PANEL_ADMIN: { to: "/admin", label: "Panel de administración" },
+      PANEL_COLLABORATORS: { to: "/admin", label: "Panel de administración" },
+      PANEL_SCHEDULES: { to: "/admin", label: "Panel de administración" },
+    },
+  },
+  API: {
+    REACT_APP_API: import.meta.env.VITE_APP_API,
+  },
+  NAVIGATE: [{ to: "/admin", label: "Panel de administración" }],
+};
+
+export default CONSTANTS;
